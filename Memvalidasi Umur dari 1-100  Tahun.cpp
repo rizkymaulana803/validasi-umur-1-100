dc07c148736d30ca1808acd9f05bf53ab47eb8ba
rizkymaulana803 <46336552+rizkymaulana803@users.noreply.github.com>
2019-01-03 19:22:37 +0700

    Add files via upload

---
+++ Memvalidasi Umur dari 1-100  Tahun.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main ()
+{
+	int umur;
+	cout<<"Masukkan Umurmu : ";
+	cin>>umur;
+	
+	if (umur>=100)
+	cout<<"wooww ternyata kamu sudah tua ya"<<endl;
+	cout<<"Jadi Umurmu : "<<umur<<"tahun";
+}
